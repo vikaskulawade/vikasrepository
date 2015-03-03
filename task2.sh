d67fd504cfd1337ffdffedf861e7ab1c0d0cf918
vikaskulawade <vikaskulawade@gmail.com>
2015-03-03 12:36:43 +0530

    all 7 task

--- task2.sh
+++ task2.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 var1="/usr/include/bits/typesizes.h"
 var2="/usr/include/linux/posix_types.h"
+var14="/home/http/FCGI_ADSERVER/Install/Apache2/conf/extra/httpd-default.conf"
+
+
 var3=$(cat $var1 |grep "__FD_SETSIZE"|cut -d'_' -f4|tr -s [\\t] ' ' | cut -d' ' -f2)
 #var3=$(cat $var1 | grep __FD_SETSIZE | tr -s '\t' ' ' |tr -s ' ' ' ' |  cat -T | cut -d' ' -f3)
 #echo var3= $var3 
@@ -20,14 +23,14 @@ fi
 
 #task2
 var5=$(ulimit -a | grep "core file size"| cut -d')' -f2)
-echo $var5
+#echo $var5
 
 var6=$(ulimit -a | grep "stack size"|cut -d')' -f2)
 #echo $var6
 size=20480
 if [ $var5 = "unlimited" -a $var6 -eq $size ]
 then
-        echo "OK"
+        echo "Both Value Matches to each other"
 else
         echo "Expected value === $ulimit and Unexpected value === $var3"
 fi
@@ -56,13 +59,12 @@ fi
 
 
 var10=$(grep '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' /etc/*.properties )
-echo $var10
+#echo $var10
 
 
 var11=$( ifconfig -a | sed '1d' |sed '2,17d'|cut -d':' -f2|awk '{print $1}'|cut -c1-6)
 
 
-echo $var11
 
 
 var12=$(echo "$var10" | grep -v "$var11"|xargs -n 1 )
@@ -71,10 +73,105 @@ var12=$(echo "$var10" | grep -v "$var11"|xargs -n 1 )
 
 if [ "$var12" ]
 then
-        echo "UnExpected $var12"
+        echo "\n  UnExpected =$var12"
 else
-       echo" ok "
+       echo" Expected    =$var12"
 fi
+
+
+
+var15=$(cat $var14 |grep "Timeout 300"|cut -d' ' -f2 )
+echo $var15
+
+var16=$(cat $var14 |grep "KeepAlive Off"|cut -d' ' -f2 )
+echo $var16
+
+
+var17=$( echo stats | nc 172.16.4.15 11211 | grep curr_conn)
+echo $var17
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
